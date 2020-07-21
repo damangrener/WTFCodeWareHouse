7aab243aaab62504d6910ea05bc845892d6ac3bf
王腾飞 <wangtengfei@znlhzl.com>
2020-07-21 15:05:53 +0800

    init

---
+++ src/main/java/com/wtf/codewarehouse/CodeWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.wtf.codewarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeWarehouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wtf/codewarehouse/CodewarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtf.codewarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodewarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
