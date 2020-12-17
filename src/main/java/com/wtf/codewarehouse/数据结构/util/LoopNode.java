dd4c213a3a13babda7abd9c9e36695b08c2147c7
王腾飞 <wangtengfei@znlhzl.com>
2020-12-17 20:57:32 +0800

    1217

---
+++ src/main/java/com/wtf/codewarehouse/数据结构/util/LoopNode.java
@@ -0,0 +1,67 @@
+package com.wtf.codewarehouse.数据结构.util;
+
+
+public class LoopNode {
+
+    int data;
+    //下一个节点
+    LoopNode next=this;
+
+    public LoopNode(int data) {
+        this.data = data;
+    }
+
+
+    //获取下一个节点
+    public LoopNode next() {
+        return this.next;
+    }
+
+    //删除下一个节点
+    public void removeNext() {
+        //取出下一个节点
+        LoopNode newNext = next.next;
+        this.next = newNext;
+    }
+
+    //插入节点
+    public void after(LoopNode node){
+        LoopNode currentNode=this;
+        LoopNode nextNode=this.next;
+        currentNode.next=node;
+        currentNode.next.next=nextNode;
+    }
+
+    public void show() {
+        LoopNode currentNode = this;
+        while (true) {
+            System.out.print(currentNode.data + " ");
+            currentNode = currentNode.next;
+            if (currentNode == null) {
+                break;
+            }
+        }
+    }
+
+    public int getData() {
+        return this.data;
+    }
+
+    public boolean isLast() {
+        return this.next == null;
+    }
+
+    public static void main(String[] args) {
+        LoopNode n1 = new LoopNode(1);
+        LoopNode n2 = new LoopNode(2);
+        LoopNode n3 = new LoopNode(3);
+        LoopNode n4 = new LoopNode(4);
+        n1.after(n2);
+        n2.after(n3);
+        n3.after(n4);
+        System.out.println(n1.next.getData());
+        System.out.println(n2.next.getData());
+        System.out.println(n3.next.getData());
+        System.out.println(n4.next.getData());
+    }
+}
--- src/main/java/com/wtf/codewarehouse/数据结构/util/Node.java
+++ src/main/java/com/wtf/codewarehouse/数据结构/util/Node.java
@@ -7,45 +7,76 @@ public class Node {
     //下一个节点
     Node next;
 
-    public Node(int data){
-        this.data=data;
+    public Node(int data) {
+        this.data = data;
     }
 
-    public Node append(Node node){
-        Node currentNode=this;
-        while(true){
-            Node nextNode=currentNode.next();
-            if(null==nextNode){
+    public Node append(Node node) {
+        Node currentNode = this;
+        while (true) {
+            Node nextNode = currentNode.next();
+            if (null == nextNode) {
                 break;
             }
-            currentNode=nextNode;
+            currentNode = nextNode;
         }
-        this.next=node;
+        currentNode.next=node;
         return this;
     }
 
     //获取下一个节点
-    public Node next(){
+    public Node next() {
         return this.next;
     }
 
-    public int getData(){
+    //删除下一个节点
+    public void removeNext() {
+        //取出下一个节点
+        Node newNext = next.next;
+        this.next = newNext;
+    }
+
+    //插入节点
+    public void after(Node node){
+        Node currentNode=this;
+        Node nextNode=this.next;
+        currentNode.next=node;
+        currentNode.next.next=nextNode;
+    }
+
+    public void show() {
+        Node currentNode = this;
+        while (true) {
+            System.out.print(currentNode.data + " ");
+            currentNode = currentNode.next;
+            if (currentNode == null) {
+                break;
+            }
+        }
+    }
+
+    public int getData() {
         return this.data;
     }
 
-    public boolean isLast(){
-        return this.next==null;
+    public boolean isLast() {
+        return this.next == null;
     }
 
     public static void main(String[] args) {
-        Node n1=new Node(1);
-        Node n2=new Node(2);
-        Node n3=new Node(3);
-        n1.append(n2);
-        n1.append(n3).append(new Node(4));
-        System.out.println(n1.next.getData());
-        System.out.println(n2.next.getData());
-        System.out.println(n1.isLast());
-        System.out.println(n3.next().isLast());
+        Node n1 = new Node(1);
+        Node n2 = new Node(2);
+        Node n3 = new Node(3);
+//        n1.append(n2);
+        n1.append(n2).append(n3).append(new Node(4));
+//        System.out.println(n1.next.getData());
+//        System.out.println(n2.next.getData());
+//        System.out.println(n1.isLast());
+//        System.out.println(n3.next().isLast());
+        n1.show();
+        n1.removeNext();
+        System.out.println();
+        n3.next().after(new Node(6));
+        n1.show();
     }
 }
